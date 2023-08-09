e5dc88107d974eaa073fc16835fe4ff545cd19b7
krishnaprasad <krishnaprasad8369@gmail.com>
2023-08-09 14:54:39 +0530

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header/Header"
 
 import Slide from "./Slide/Slide"
-import'./app.css'
+
 import Footer from "./Footer/Footer";
 import Cards from "./Cards/Cards";
 import Footerimage from "./Footerimage/Footerimage";
 import Navbar from "../src/NavBar/Navbar/";
+
+
 
 
 
