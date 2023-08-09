81b5947721860163b4b975289297e27b2fd23be4
krishnaprasad <krishnaprasad8369@gmail.com>
2023-08-09 15:09:44 +0530

    card

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import Header from "./Header/Header"
 
-import Slide from "./Slide/Slide"
-
-import Footer from "../src/Footer/Footer";
-import Cards from "../src/Cards/Cards";
-import Footerimage from "../src/Footerimage/Footerimage";
-import Navbar from "../src/NavBar/Navbar/";
+import './App.css'
 
 
+
+import Home from "./Home";
 
 
 
@@ -17,12 +13,7 @@ function App() {
 
   return (
     <>
-   <Navbar/>
-   <Header/>
-   <Slide/>
-   <Cards/>
-   <Footerimage/>
-   <Footer/>
+  <Home/>
   
       
     </>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
+import Header from './Header/Header'
+import Slide from './Slide/Slide'
+import Cards from '../src/Cards/Cards'
+import Footerimage from './Footerimage/Footerimage'
+import Footer from './Footer/Footer'
 
 function Home() {
   return (
     <div>
+     
         <Navbar/>
-        
+   <Header/>
+   <Slide/>
+   <Cards/>
+   <Footerimage/>
+   <Footer/>
+
     </div>
   )
 }
