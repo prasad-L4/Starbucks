2355967e7c6a28e3da94a77fc0e51b728a4ac542
krishnaprasad <krishnaprasad8369@gmail.com>
2023-08-09 15:00:17 +0530

    appjs updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Header from "./Header/Header"
 
 import Slide from "./Slide/Slide"
 
-import Footer from "./Footer/Footer";
-import Cards from "./Cards/Cards";
-import Footerimage from "./Footerimage/Footerimage";
+import Footer from "../src/Footer/Footer";
+import Cards from "../src/Cards/Cards";
+import Footerimage from "../src/Footerimage/Footerimage";
 import Navbar from "../src/NavBar/Navbar/";
 
 
