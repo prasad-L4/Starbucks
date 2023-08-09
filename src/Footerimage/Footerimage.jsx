155a0dae0e30cab08edf5ab17b2ce9e9900b8fd4
krishnaprasad <krishnaprasad8369@gmail.com>
2023-08-09 16:41:35 +0530

    made responsive

--- src/Footerimage/Footerimage.jsx
+++ src/Footerimage/Footerimage.jsx
@@ -8,10 +8,10 @@ const Footerimage = () => {
         <div>
             <p className='le-head'>Learn more about world of coffe!</p>
             <div className="header1">
-          <img className="image1" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF9TBSk6m6npsgY6v_sKzde6S2XWis94hHC7kXxdfSNDrIgJvu8UruL7iGHw0mER_J93g&usqp=CAU' alt="" />
+          <img className="image1" src='https://preprodtsbstorage.blob.core.windows.net/cms/uploads/Coffeculture_Blog_Web_Banner_2_X_8b72db6546.jpg' alt="" />
           <div className="overlay1">
-            <h1 className='text-4'>Coffe-Origin Story :Mystic,Intriguing And Armotic</h1>
-            <h3 className='text3'>Many certuries ago,somewhere between 800-900 AD the story of coffe first took root in Ethiopia.</h3>
+            <h1 className='text-4'>Coffe-Origin Story :Mystic,Intriguing <p className='armotic'>And Armotic</p></h1>
+            <h3 className='text3'>Many certuries ago,somewhere between 800-900 AD the story of coffe <p className='first-took'>first took root in Ethiopia.</p> </h3>
             
             <button className="btn" variant="light">
               Learn More
